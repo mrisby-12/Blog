7d206a24ecb729408d97231fea8e32fff12948ff
michelle risby <michelle@anedot.com>
2018-07-20 12:59:17 -0500

    Added Pages routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717012915) do
+ActiveRecord::Schema.define(version: 2018_07_20_173507) do
 
   create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "post_id"
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text     "body"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
