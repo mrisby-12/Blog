b975f127a07322ba2a665cd6d6e7454027c19f80
michelle risby <michelle@anedot.com>
2018-07-23 16:25:42 -0500

    Add `link_to` to make routes to individual page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     resources :comments
   end
   get '/pages', to: 'pages#index'
+  get '/pages/:id', to: 'pages#show', as: 'page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
