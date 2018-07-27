e8ea4701034e005876029f0dd1790015f2e34b0d
michelle risby <michelle@anedot.com>
2018-07-27 12:16:50 -0500

    Added delete and completed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  get '/pages', to: 'pages#index'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show', as: 'page'
-  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  patch '/pages/:id', to: 'pages#update'
-  post '/pages', to: 'pages#create'
+  resources :pages
 end
